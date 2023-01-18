ce46b4e89900579a8fc9a82e634d059b25bb1601
Coolfire213 <103848381+Coolfire213@users.noreply.github.com>
2023-01-17 20:15:21 -0500

    shader commit

---
+++ GameProject/VAO.h
@@ -0,0 +1,41 @@
+#ifndef VAO_CLASS_H
+#define VAO_CLASS_H
+
+#include "include/glew.h"
+#include "VBO.h"
+
+class VAO
+{
+	public:
+		GLuint ID;
+		VAO()
+		{
+			glGenVertexArrays(1, &ID);
+		}
+
+		void LinkVBO(VBO VBO, GLuint layout)
+		{
+			VBO.Bind();
+			glVertexAttribLPointer(layout, 3, GL_FLOAT, 0, (void*)0);
+			glEnableVertexAttribArray(layout);
+			VBO.Unbind();
+		}
+
+		void Bind()
+		{
+			glBindVertexArray(ID);
+		}
+
+		void Unbind()
+		{
+			glBindVertexArray(0);
+		}
+
+		void Delete()
+		{
+			glDeleteVertexArrays(1, &ID);
+		}
+};
+
+#endif // !VAO_CLASS_H
+
--- GameProject/src/main.cpp
+++ GameProject/src/main.cpp
@@ -2,6 +2,16 @@
 #include "glfw3.h"
 #include <cassert>
 
+#include "shader.h"
+#include "VAO.h"
+
+//temp
+GLfloat vertices[] = {
+	-1.0f, -1.0f, 0.0f,
+	 1.0f, -1.0f, 0.0f,
+	 1.0f,  1.0f, 0.0f
+};
+
 int  main(int argc, char** argv)
 {
 	GLFWwindow* window;
@@ -29,11 +39,22 @@ int  main(int argc, char** argv)
 
 	glfwMakeContextCurrent(window);
 
+	Shader shaderProgram("vertex.vs","fragment.fs");
+	VAO VAO1;
+	VAO1.Bind();
+	VBO VBO1(vertices, sizeof(vertices));
+
+	//Links VBO to VAO
+	VAO1.LinkVBO(VBO1, 0);
+
+	VAO1.Unbind();
+	VBO1.Unbind();
+
 	while (!glfwWindowShouldClose(window))
 	{
 		glClear(GL_COLOR_BUFFER_BIT);
-		glUseProgram(shaderProgram);
-		glBindVertexArray(VAO);
+		shaderProgram.use();
+		VAO1.Bind();
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 		glfwSwapBuffers(window);
 		glfwPollEvents();
